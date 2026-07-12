111b6c07a66bb7529e34d9fe98be9097ab6e102c
gavin-nowlin <gavinnowlin3@gmail.com>
2026-07-12 16:32:03 -0400

    Updated website and added favicons

--- web/src/data/site.ts
+++ web/src/data/site.ts
@@ -4,7 +4,7 @@ export const site = {
 	description:
 		'Software developer and co-founder of The Vision Devs. Based in Florida.',
 	url: 'https://gavinnowlin.com',
-	bio: 'I build software and websites for The Vision Devs and I enjoy working on games in my custom game engine I\'m currently building.',
+	bio: 'I build software and websites for The Vision Devs. I also enjoy working on games in my own game engine I\'m building.',
 	role: 'Co-founder & Lead Software Dev',
 	company: {
 		name: 'The Vision Devs',
